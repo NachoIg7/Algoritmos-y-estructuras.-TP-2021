1d8e9c114a328423f1bef6dd9e8c391f85e29c68
NachoIg7 <81595133+NachoIg7@users.noreply.github.com>
2021-05-29 14:45:59 -0300

    Add files via upload

--- ejercicios-pila.py
+++ ejercicios-pila.py
@@ -54,37 +54,46 @@ from random import randint
 
 
 
-pila1 = Pila()
-pila2 = Pila()
-pila_repetido = Pila()
-pila1_aux = Pila()
-pila2_aux = Pila()
+# pila1 = Pila()
+# pila2 = Pila()
+# pila_repetido = Pila()
+# pila1_aux = Pila()
 
-for i in range (0,5):
-    personaje = str(input ('Ingrese personaje del episodio v de star wars: '))
-    pila1.apilar(personaje)
+# #Cargamos personajes pila 1
+# pila1.apilar('enano verde')
+# pila1.apilar('chiwi')
+# pila1.apilar('arturo')
+
+# #Cargamos personajes pila 2
+# pila2.apilar('droide')
+# pila2.apilar('tenebroso')
+# pila2.apilar('enano verde')
 
 
-
-for p in range (0,5):
-    personaje = str(input ('Ingrese personaje del episodio vii de star wars: '))
-    pila2.apilar(personaje)
+    
 
 
-while(not pila1.pila_vacia):
-    if (pila1.elemento_cima() == pila2.elemento_cima()):
-        pila_repetido.apilar(pila1.desapilar)
-    else:
-        pila1_aux.apilar(pila1.desapilar)
+# while(not pila1.pila_vacia()):
+#     aux=pila1.desapilar()
+#     if ( aux == pila2.elemento_cima()):
+#         pila_repetido.apilar(aux)
+#         pila2.desapilar()
+#         print ('personaje repetido cargado')
+#     else:
+#         pila1_aux.apilar(aux)
+#         print ('no hay coincidencia')
 
-while(not pila2.pila_vacia):
-    if (pila2.elemento_cima() == pila1_aux.elemento_cima()):
-        pila_repetido.apilar(pila2.desapilar)
-    else:
-        pila2_aux.apilar(pila2.desapilar)
+# while(not pila2.pila_vacia() and not pila1_aux.pila_vacia()):
+#     aux=pila2.desapilar()
+#     if (aux == pila1_aux.elemento_cima()):
+#         pila_repetido.apilar(aux)
+#         print ('personaje repetido cargado')
+#     else:
+    
+#         print ('no hay coincidencia')
 
-while (not pila_repetido.pila_vacia()):
-    print (pila_repetido.desapilar())
+# while (not pila_repetido.pila_vacia()):
+#     print (pila_repetido.desapilar())
 
 
 
@@ -116,8 +125,88 @@ while (not pila_repetido.pila_vacia()):
 # que Boba Fett capturo a Han Solo, suponga que dicha misión está cargada;
 # d. indicar la cantidad de capturas realizadas por cada cazarrecompensas.
 
+# class Bitacora(object):
+#     planeta, capturado, recompensa = '', '', float
+
+#     def __init__(self, planeta, capturado, recompensa):
+#          self.planeta = planeta
+#          self.capturado = capturado
+#          self.recompensa = recompensa
+
+#     def __str__(self):
+#          return self.planeta+' - '+self.capturado+' - '+self.recompensa
+
+# boba = Pila()
+# djarin = Pila()
+
+# #cargamos bitacora de Boba Fett
+
+# dato = Bitacora ('tierra','han solo',10000000)
+# boba.apilar(dato)
+# dato = Bitacora ('rojo','droide',200)
+# boba.apilar(dato)
+# dato = Bitacora ('azul','enano verde',10)
+# boba.apilar(dato)
+# dato = Bitacora ('mercurio','asesino',5542)
+# boba.apilar(dato)
 
 
+# #cagamos bitacora de Din Djarin
+# dato = Bitacora ('257','soldado',4478)
+# djarin.apilar(dato)
+# dato = Bitacora ('900','luke',845600)
+# djarin.apilar(dato)
+# dato = Bitacora ('7882','lela',420)
+# djarin.apilar(dato)
+
+
+# contador_capturas_boba = 0
+# contador_capturas_djarin = 0
+# captura = False
+# num_mision = 0
+# cont_mision = 0
+# mision_captura = 0
+# acum_boba = float
+# acum_djarin = float
+
+# print ('Planetas visitados por Boba Fett')
+# acum_boba = 0
+# while (not boba.pila_vacia()):
+#     x = boba.desapilar()
+#     contador_capturas_boba = contador_capturas_boba + 1
+#     cont_mision = cont_mision + 1
+#     acum_boba = acum_boba + x.recompensa
+#     print (x.planeta)
+        
+
+#     if (x.capturado == 'han solo'):
+#         captura = True
+#         mision_captura = cont_mision
+
+# print ('En total realizo ' + str(contador_capturas_boba) + ' capturas')
+
+# print ('Recaudó ' + str(acum_boba) + ' creditos galacticos')
+
+
+# print ('Planetas visitados por Din Djarin')
+# acum_djarin = 0
+# while (not djarin.pila_vacia()):
+#     x = djarin.desapilar()
+#     contador_capturas_djarin = contador_capturas_djarin + 1
+#     acum_djarin = acum_djarin + x.recompensa
+#     print (x.planeta)
+
+# print ('En total realizo ' + str(contador_capturas_djarin) + ' capturas')
+
+# print ('Recaudó ' + str(acum_djarin) +  ' creditos galacticos')
+
+# if (acum_boba > acum_djarin):
+#     print ('El mercenario Boba Fett acumulo mas creditos galacticos')
+# else:
+#     print ('El mercenario Din Djarin acumulo mas creditos galacticos')
+
+# if (captura):
+#     print ('Boba Fett capturo a han solo en la mision ' + str(mision_captura))
 
 
 
@@ -126,7 +215,66 @@ while (not pila_repetido.pila_vacia()):
 #Dada una pila de personajes de Marvel Cinematic Universe (MCU), de los cuales se dispone de 
 # su nombre y la cantidad de películas de la saga en la que participó, implementar las funciones 
 # necesarias para resolver las siguientes actividades:
-# a. determinar en qué posición se encuentran Rocket Raccoon y Groot, tomando como posición uno la cima de la pila;[86]
+# a. determinar en qué posición se encuentran Rocket Raccoon y Groot, tomando como posición uno la cima de la pila;
 # b. determinar los personajes que participaron en más de 5 películas de la saga, además indicar la cantidad de películas en la que aparece;
 # c. determinar en cuantas películas participo la Viuda Negra (Black Widow);
 # d. mostrar todos los personajes cuyos nombre empiezan con C, D y G.
+
+class Personaje (object):
+        nombre, cant_peliculas = '' , int
+        
+        def __init__(self, nombre, cant_peliculas):
+            self.nombre = nombre
+            self.cant_peliculas = cant_peliculas
+        
+        def __str__(self):
+            return self.nombre + '-' +self.cant_peliculas
+
+personaje = Pila()
+
+dato = Personaje ('iron man', 6)
+personaje.apilar(dato)
+dato = Personaje ('capitan america', 5)
+personaje.apilar(dato)
+dato = Personaje ('hulk', 9)
+personaje.apilar(dato)
+dato = Personaje ('hormiga', 3)
+personaje.apilar(dato)
+dato = Personaje ('black widow', 4)
+personaje.apilar(dato)
+dato = Personaje ('groot', 5)
+personaje.apilar(dato)
+dato = Personaje ('rocket raccoonn', 5)
+personaje.apilar(dato)
+dato = Personaje ('thor', 6)
+personaje.apilar(dato)
+dato = Personaje ('pantera', 3)
+personaje.apilar(dato)
+dato = Personaje ('fury', 20)
+personaje.apilar(dato)
+dato = Personaje ('thanos', 2)
+personaje.apilar(dato)
+dato = Personaje ('ford falcon', 1)
+personaje.apilar(dato)
+
+
+cont_posicion = 0  
+
+while (not personaje.pila_vacia()):
+    x = personaje.desapilar()
+    cont_posicion = cont_posicion + 1
+
+    if (x.nombre == 'rocket raccoonn'):
+        print ('rocket raccoonn esta en la posicion ' + str(cont_posicion))
+    if (x.nombre == 'groot'):
+        print ('groot esta en la posicion ' + str(cont_posicion)) 
+
+    if (x.cant_peliculas >= 5):
+        print (x.nombre + ', participo en mas de 5 peliculas. En total: ' + str(x.cant_peliculas))   
+
+    if (x.nombre == 'black widow'):
+        print ('black widow participo en ' + str(x.cant_peliculas) + ' peliculas' )
+
+    if (x.nombre[0] == 'c' or x.nombre[0] == 'd' or x.nombre[0] == 'g'):
+        print ('personaje comenzado con letra c,d o g ')
+        
